6ede7a1f5a736507d3f11324819cd66943355284
romaindudek <pro@osezmedia.com>
2022-09-21 12:39:15 +0200

    Refactor and clean

--- welcomeDiags.sh
+++ welcomeDiags.sh
@@ -15,6 +15,10 @@ upfrom=$(uptime -p)
 
 totalDiskSpace=$(df -h /dev/mapper/vg00-var | grep /dev/ | awk '{print $2}')
 rawFreeDiskSpace=$(df /dev/mapper/vg00-var | grep /dev/ | awk '{print $4/$2 *100}')
+[ "$rawFreeDiskSpace" != "inf" ] && freeDiskSpace=${rawFreeDiskSpace%.*} || freeDiskSpace='100'
+
+totalDiskSpace=$(df -h / | grep /dev/ | awk '{print $2}')
+rawFreeDiskSpace=$(df / | grep /dev/ | awk '{print $4/$2 *100}')
 freeDiskSpace=${rawFreeDiskSpace%.*}
 
 
@@ -23,24 +27,20 @@ cachedRam=$(free -m | grep Mem | awk '{print $6}')
 swapTotal=$(free -m | grep Swap | awk '{print $2}')
 rawSwapFree=$(free -m | grep Swap | awk '{print $4/$2 *100}')
 rawFreeMemory=$(free -m | grep Mem | awk '{print $4/$2 *100}')
+[ "$rawFreeMemory" != "inf" ] && freeMemory=${rawFreeMemory%.*} || freeMemory='100'
 freeMemory=${rawFreeMemory%.*}
-[[ "$rawSwapFree" =~ ([0-9])+.?\w ]] && freeSwap=${rawSwapFree%.*} || freeSwap=''
+[ "$rawSwapFree" != "inf" ] && freeSwap=${rawSwapFree%.*} || freeSwap='100'
 
 ipAddr=$(hostname -I)
-[ -f "/sys/class/thermal/thermal_zone0/temp" ] && procTemp=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{ print $1/1000}')
-[ -f "/sys/class/thermal/thermal_zone1/temp" ] && ramTemp=$(cat /sys/class/thermal/thermal_zone1/temp | awk '{ print $1/1000}')
+[ -f "/sys/class/thermal/thermal_zone0/temp" ] && procTemp=$( awk '{ print $1/1000}' < /sys/class/thermal/thermal_zone0/temp)
+[ -f "/sys/class/thermal/thermal_zone1/temp" ] && ramTemp=$( awk '{ print $1/1000}' < /sys/class/thermal/thermal_zone1/temp)
 
-PrintDiag "Disk space" "${freeDiskSpace}%% free over ${totalDiskSpace}              "
-PrintDiag "Up time" "${upfrom}\n"
-PrintDiag "RAM" "${freeMemory}%% free over ${totalMemory}Mb, ${cachedRam}Mb in cache      "
-PrintDiag "Swap" "${freeSwap}%% free over ${swapTotal}Mb\n"
-PrintDiag "Proc temp" "${procTemp}°C      "
-PrintDiag "RAM temp" "${ramTemp}°C       "
-PrintDiag "Ip adrr" "${ipAddr}\n"
-# [ ! -z "$freeDiskSpace" ] && printf "${CYAN}Disk space :${NC} ${freeDiskSpace}%% free over ${totalDiskSpace}              "
-# [ ! -z "$upfrom" ] && printf "${CYAN}Up time :${NC}  ${upfrom}\n"
-# [ ! -z "$freeMemory" ] && printf "${CYAN}RAM :${NC} ${freeMemory}%% free over ${totalMemory}Mb, ${cachedRam}Mb in cache      "
-# [ ! -z "$freeSwap" ] && printf "${CYAN}Swap :${NC} ${freeSwap}%% free over ${swapTotal}Mb\n"
-# [ ! -z "$procTemp" ] && printf "${CYAN}Proc temp :${NC} ${procTemp}°C      ${CYAN}RAM temp :${NC} ${ramTemp}°C       "
-# [ ! -z "$ipAddr" ] && printf "${CYAN}Ip adrr :${NC} ${ipAddr}\n"
+PrintDiag "Disk space" "${freeDiskSpace}% free over ${totalDiskSpace}"
+PrintDiag "Up time" "${upfrom}" "nl"
+PrintDiag "RAM" "${freeMemory}% free over ${totalMemory}Mb, ${cachedRam}Mb in cache"
+PrintDiag "Swap" "${freeSwap}% free over ${swapTotal}Mb" "nl"
+PrintDiag "Proc temp" "${procTemp}°C"
+PrintDiag " RAM temp" "${ramTemp}°C" "nl"
+PrintDiag "Ip adrr" "${ipAddr}" "nl"
+
 
